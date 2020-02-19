d2ae141a181a87106d7b1a1b9da6e432dc374254
Daniel <danielsalasvalle@gmail.com>
2020-02-19 12:24:52 +0100

    Esto es una prueba para saber que ser usar esto

--- Libreria_Gasolinera/Libreria_Gasolinera/Empleado.vb
+++ Libreria_Gasolinera/Libreria_Gasolinera/Empleado.vb
@@ -1,3 +1,34 @@
 ﻿Public Class Empleado
 
+    Dim id As Integer
+    Dim dni As String
+    Dim nombre As String
+    Dim apellidos As String
+    Dim rol As String
+    Dim telefono As String
+    Dim email As String
+    Dim direccion As String
+
+    Public Sub New(ByVal id As Integer, ByVal dni As String, ByVal nombre As String, ByVal apellidos As String, ByVal rol As String)
+        Me.id = id
+        Me.dni = dni
+        Me.nombre = nombre
+        Me.apellidos = apellidos
+        Me.rol = rol
+    End Sub
+
+
+    Public Sub New(ByVal id As Integer, ByVal dni As String, ByVal nombre As String, ByVal apellidos As String, ByVal rol As String, ByVal telefono As String, ByVal email As String, ByVal direccion As String)
+        Me.id = id
+        Me.dni = dni
+        Me.nombre = nombre
+        Me.apellidos = apellidos
+        Me.rol = rol
+        Me.telefono = telefono
+        Me.email = email
+        Me.direccion = direccion
+    End Sub
+
+
+
 End Class
