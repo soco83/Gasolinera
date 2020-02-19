18bd37124cb4fe897d649df679da30c02f28ae35
Daniel <danielsalasvalle@gmail.com>
2020-02-19 20:40:21 +0100

    He dado forma al formulario de inicio

---
+++ Gasolinera_r/Gasolinera_r/Inicio.vb
@@ -0,0 +1,14 @@
+﻿Public Class Inicio
+
+
+    Private Sub Inicio_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        lContinuar.Visible = False
+        pRegistro.Visible = True
+    End Sub
+
+    Private Sub lContinuar_Click(sender As Object, e As EventArgs) Handles lContinuar.Click
+
+    End Sub
+
+
+End Class
